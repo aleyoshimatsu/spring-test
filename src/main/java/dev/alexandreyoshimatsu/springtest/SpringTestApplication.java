74ee8e16d5e9b349e6df39a71161f8cccd862b08
Alexandre Pantalena Yoshimatsu <aleyoshimatsu@gmail.com>
2020-05-23 18:05:50 -0300

    Adicionando gradle-release plugin

--- src/main/java/dev/alexandreyoshimatsu/springtest/SpringTestApplication.java
+++ src/main/java/dev/alexandreyoshimatsu/springtest/SpringTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"dev.alexandreyoshimatsu"})
-public class SprintTestApplication {
+public class SpringTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprintTestApplication.class, args);
+		SpringApplication.run(SpringTestApplication.class, args);
 	}
 
 }
